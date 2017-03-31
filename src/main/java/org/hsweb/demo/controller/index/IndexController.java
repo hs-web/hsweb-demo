c8f7be0bfd05daf66e912a3fbfe08299efb27854
zhouhao <zh.sqy@qq.com>
2017-03-31 16:25:47 +0800

    优化

--- src/main/java/org/hsweb/demo/controller/index/IndexController.java
+++ src/main/java/org/hsweb/demo/controller/index/IndexController.java
@@ -1,8 +1,12 @@
 package org.hsweb.demo.controller.index;
 
 import org.hsweb.web.core.authorize.annotation.Authorize;
+import org.hsweb.web.core.utils.WebUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author zhouhao
@@ -11,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping(value = {"/", "/index.html"})
     @Authorize
-    public String index() {
-        return "admin/index";
+    public ModelAndView index(HttpServletRequest request) {
+        ModelAndView view = new ModelAndView("admin/index");
+        view.addObject("absPath", WebUtil.getBasePath(request));
+        return view;
     }
 }
